d389275e847ddddbb4b9795a20716bf47a912f8c
WebDevGabi <gabiinreal@gmail.com>
2024-05-16 16:10:14 +0200

    Connecting to the database.

---
+++ db/connect.js
@@ -0,0 +1,11 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const db = 'ExpressUserManagement'
+
+module.exports = async () => {
+    try {
+        const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+        global.db = client.db(db);
+    } catch (e) {}
+}
--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const app = express();
+const CORS = require('cors');
+const connect = require('./db/connect');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const app = express();
+app.use(express.json())
+    .use(express.urlencoded({ extended: true }))
+    .use(CORS());
+
+connect();
 
 app.listen(3000, () => console.log('http://localhost:3000'));
